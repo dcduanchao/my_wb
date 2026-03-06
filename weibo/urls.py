7b4a76fede8da7227d0a4379081d6a167a64bf66
ext.duanchao5 <duanchao@xiaoneng.cn>
2026-03-06 15:45:44 +0800

    restore code from 0481644 and remove API key

--- weibo/urls.py
+++ weibo/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 
-from weibo import views,test_views,comfyui_views
-
-
+from weibo import views, test_views, comfyui_views, grok_views
 
 app_name = 'my_weibo'
 urlpatterns = [
@@ -24,6 +22,11 @@ urlpatterns = [
     path("comfyui/image/delete", comfyui_views.comfyui_image_delete, name="comfyui_image_delete"),
     path("comfyui/create", comfyui_views.comfyui_create, name="comfyui_create"),
 
+
+    # grok
+    path("grok/gen", grok_views.grok_gen, name="grok_gen"),
+    path("grok/list", grok_views.grok_list, name="grok_list"),
+    path("grok/delete", grok_views.grok_delete, name="grok_deelete"),
 
 
 
